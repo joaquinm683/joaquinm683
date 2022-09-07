046f0d21d6f7ed9303129e2a8277659367710d8f
Joaquin <joaquinm683@gmail.com>
2022-09-07 10:44:46 -0500

    Se agrega prepared statment

--- src/main/java/com/isil/sesion1/MainApp.java
+++ src/main/java/com/isil/sesion1/MainApp.java
@@ -1,15 +1,17 @@
 package com.isil.sesion1;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MainApp {
 
     public static void main(String[] args) throws Exception {
         //System.out.println("Hello World");
-
+        // 1. Cargar drive
+        // 2. crear conexion
+        //3 creat statement
+        //4. ejecutar query
+        //5. recorrer resultatods
+        //6. Cerrar conexion
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/TEST",
@@ -19,16 +21,46 @@ public class MainApp {
         Statement stmt = con.createStatement();
         int resultUpdate = stmt.executeUpdate("update Users set City='Rosario' where name= 'Messi'");
 
-        ResultSet result = stmt.executeQuery("select * from Users where name='Messi'");
 
-        while(result.next()){
 
-            System.out.println(result.getString("idUser"));
-            System.out.println(result.getString("name"));
-            System.out.println(result.getString("phone"));
-            System.out.println(result.getString("city"));
+        Statement stmt2 = con.createStatement();
 
+        ResultSet resultSet = stmt2.executeQuery("select * from Users where name ='Ronaldo'");
+        while(resultSet.next()){
+
+            System.out.println(resultSet.getString("name") + "\n" +
+                    ""+ resultSet.getString("phone") + "\n"+
+                    resultSet.getString("city")
+            );
         }
+
+        PreparedStatement preparedStatement = con.prepareStatement("select * from Users where name=? and city=?");
+
+        preparedStatement.setString(1,"Cueva");
+        preparedStatement.setString(2,"Lima");
+        ResultSet resultSet2 = preparedStatement.executeQuery();
+
+        Statement stCreate = con.createStatement();
+        int affectedRows = stCreate.executeUpdate("UPDATE Users set name='juan' where iduser=1");
+
+        System.out.println("Affected Rows" + affectedRows);
+      PreparedStatement preparedStatement1 = con.prepareStatement("select * from Users where iduser=?");
+      preparedStatement1.setInt(1,1);
+
+      ResultSet resultSet3= preparedStatement1.executeQuery();
+
+
+
+
+
+        while(resultSet3.next()){
+
+           System.out.println(resultSet3.getString("name") + "\n" +
+                   ""+ resultSet3.getString("phone") + "\n"+
+                    resultSet3.getString("city")
+           );
+        }
+        con.close();
     }
 
 
